9a3669040fa12faafc218af95c5051effd1612fa
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2021-08-17 20:42:21 -0700

    fixed bug in mobile version

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
 const hamburger = document.querySelector(".hamburger");
 const close = document.querySelector(".close");
-
 let navigation = document.querySelector(".navUl");
+const upArrow = document.querySelector(".upArrow");
+
+
+
+
+
 
 hamburger.addEventListener("click", () => {
     navigation.classList.add("open");
@@ -10,3 +15,24 @@ hamburger.addEventListener("click", () => {
 close.addEventListener("click", function () {
     navigation.classList.remove("open");
 })
+
+window.addEventListener("scroll", e => {
+
+    if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+        upArrow.classList.add("visible");
+    } else {
+        upArrow.classList.remove("visible");
+    }
+});
+
+
+// navigation on click remove mobile navigation 
+const list = document.querySelectorAll(".list");
+
+
+for (let i = 0; i < list.length; i++) {
+    list[i].addEventListener("click", function () {
+        navigation.classList.remove("open");
+
+    })
+}
